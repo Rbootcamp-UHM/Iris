f6677b5f98c93a4cfea03e941fc4d9614680a2ce
Raine Higa <RaineFija@Raines-MacBook-Pro.local>
2020-01-19 11:50:07 -1000

    Changes made

--- markdown_rh.R
+++ markdown_rh.R
@@ -34,7 +34,7 @@ These are the names of each column.
 names(iris)
 ```
 
-Here, I am creating easy to remember variables for each vector, so I don't have to pull the vectors every time.
+Here, I am creating easy to remember variables for each vector, so I do not have to pull the vectors every time.
 
 ```{r}
 ## Accessing vectors within iris, saving as separate objects
@@ -45,20 +45,55 @@ sepalL <- iris$Sepal.Length
 sepalW <- iris$Sepal.Width
 ```
 
-This is the r code for my scatter plot 2-D graphs at the bottom.
+This is the r code for my 2-D scatter plots.
 
 ```{r}
-# Polygon scatter plot
+# Scatter plots
 color = rep("light green", 150)
 color[species == "setosa"] <- "light blue"
 color[species == "versicolor"] <- "pink"
 
 plot(petalL, petalW, col=color, pch=8, main = "Comparing Petal Length and Width of Iris Species", xlab="petal length", ylab="petal width")
 legend('bottomright', legend = levels(iris$Species), col = c("light blue", "pink", "light green"), pch=8)
-polygon(petalL, petalW)
 
 plot(sepalL, sepalW, col=color, pch=8, main = "Comparing Sepal Length and Width of Iris Species", xlab="sepal length", ylab="sepal width")
 legend('bottomright', legend = levels(iris$Species), col = c("light blue", "pink", "light green"), pch=8)
-polygon(sepalL, sepalW)
+```
+
+These are my plots using the polygon function to outline each species.
+
+```{r}
+color = rep("light green", 150)
+color[species == "setosa"] <- "light blue"
+color[species == "versicolor"] <- "pink"
+
+plot(petalL, petalW, col=color, pch=8, main = "Comparing Petal Length and Width of Iris Species", xlab="petal length", ylab="petal width")
+legend('bottomright', legend = levels(iris$Species), col = c("light blue", "pink", "light green"), pch=8)
+
+# Petal polygons
+ii.set <- chull(petalL[1:50], petalW[1:50])
+polygon(petalL[ii.set], petalW[ii.set])
+
+ii.ver <- chull(petalL[species == "versicolor"], petalW[species == "versicolor"])
+polygon(petalL[species == "versicolor"][ii.ver], petalW[species == "versicolor"][ii.ver])
+
+ii.vir <- chull(petalL[species == "virginica"], petalW[species == "virginica"])
+polygon(petalL[species == "virginica"][ii.vir], petalW[species == "virginica"][ii.vir])
+
+# Sepal polygons
+plot(sepalL, sepalW, col=color, pch=8, main = "Comparing Sepal Length and Width of Iris Species", xlab="sepal length", ylab="sepal width")
+legend('bottomright', legend = levels(iris$Species), col = c("light blue", "pink", "light green"), pch=8)
+
+vv.set <- chull(sepalL[1:50], sepalW[1:50])
+polygon(sepalL[vv.set], sepalW[vv.set])
+
+vv.ver <- chull(sepalL[species == "versicolor"], sepalW[species == "versicolor"])
+polygon(sepalL[species == "versicolor"][vv.ver], sepalW[species == "versicolor"][vv.ver])
+
+vv.vir <- chull(sepalL[species == "virginica"], sepalW[species == "virginica"])
+polygon(sepalL[species == "virginica"][vv.vir], sepalW[species == "virginica"][vv.vir])
 ```
 </body>
+
+# require(rmarkdown)
+# render("markdown_rh.Rmd", output_format="html_document")
